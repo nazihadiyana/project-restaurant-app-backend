6479b55ea97902aa5302ce2d8b21c07e84cd37b1
Naziha Diyanatur Rosiyah <101118991+nazihadiyana@users.noreply.github.com>
2022-08-15 05:49:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,12 +43,3 @@ const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-// https://www.youtube.com/watch?v=GK2TiAAxmQ0
-// https://www.bezkoder.com/node-js-rest-api-express-mysql/
-// https://www.bezkoder.com/serve-vue-app-express/
-// https://www.bezkoder.com/deploy-node-js-app-heroku-cleardb-mysql/
-// https://www.youtube.com/watch?v=W-b9KGwVECs
-// https://stackoverflow.com/questions/43362014/heroku-no-default-language-could-be-detected-for-this-app-error-thrown-for-no
-// https://stackoverflow.com/questions/16128395/what-is-procfile-and-web-and-worker
-// https://www.youtube.com/watch?v=lwOsI8LtVEQ
